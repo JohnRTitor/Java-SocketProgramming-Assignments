24d54417f79fd74e2f31cd3b03093541d0cc836c
Masum Reza <50095635+JohnRTitor@users.noreply.github.com>
2026-04-09 18:51:46 +0530

    Add comments, restructure

--- src/graph/GraphReachabilityChecker.java
+++ src/graph/GraphReachabilityChecker.java
@@ -5,147 +5,167 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 class GraphReachabilityChecker {
-	static int nVertices = 0;
-	static int[][] adj = new int[100][100];
-	static boolean[] visited = new boolean[100];
+    static int nVertices = 0;
+    static int[][] adj = new int[100][100];
 
-	static void readGraph(String filePath) {
-		try {
-			Scanner file = new Scanner(new File(filePath));
+    static void readGraph(String filePath) {
+        try (Scanner file = new Scanner(new File(filePath))) {
+            // in each line of the file: an edge (u, v) is stored like this:
+            // 2 4
+            // 4 3
+            while (file.hasNextInt()) {
+                int u = file.nextInt();
+                int v = file.nextInt();
 
-			while (file.hasNextInt()) {
-				int u = file.nextInt();
-				int v = file.nextInt();
+                // directed graph, so add edge from u to v
+                adj[u][v] = 1;
+                // vertices start from the number 0, so add 1 to max of u or v, ie
+                // if (4, 5) is found, there must be 5 + 1 = 6 vertices (0, 1, 2, 3, 4, 5)
+                nVertices = Math.max(nVertices, Math.max(u, v) + 1);
+            }
 
-				adj[u][v] = 1;
-				nVertices = Math.max(nVertices, Math.max(u, v) + 1);
-			}
+        } catch (FileNotFoundException e) {
+            System.err.println("Error: " + e.getMessage());
+            System.exit(0);
+        }
+    }
 
-			file.close();
-		} catch (FileNotFoundException e) {
-			System.out.println("File not found.");
-			System.exit(0);
-		}
-	}
+    static void printAdjacencyMatrix() {
+        // Print column headers
+        System.out.print("\t");
+        for (int i = 0; i < nVertices; i++) {
+            System.out.print("v" + i + "\t");
+        }
+        System.out.println();
 
-	static void printAdjacencyMatrix() {
-		// Print column headers
-		System.out.print("\t");
-		for (int i = 0; i < nVertices; i++) {
-			System.out.print("v" + i + "\t");
-		}
-		System.out.println();
+        for (int i = 0; i < nVertices; i++) {
+            // Print row header
+            System.out.print("v" + i + "\t");
 
-		for (int i = 0; i < nVertices; i++) {
-			// Print row header
-			System.out.print("v" + i + "\t");
+            for (int j = 0; j < nVertices; j++) {
+                System.out.print(adj[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
 
-			for (int j = 0; j < nVertices; j++) {
-				System.out.print(adj[i][j] + "\t");
-			}
-			System.out.println();
-		}
-	}
+    static void dfsComponent(int v, boolean[] visited, int[] comp, int[] size) {
+        visited[v] = true;
+        // add the vertex to the comp array
+        comp[size[0]++] = v;
 
-	static void dfsComponent(int v, int[] comp, int[] size) {
-		visited[v] = true;
-		comp[size[0]++] = v;
+        for (int i = 0; i < nVertices; i++) {
+            // if there is an edge from vertex v to i or vice versa
+            // they are in the same component, and perform DFS recursively
+            // if and only if the vertex i is not visited
+            if ((adj[v][i] == 1 || adj[i][v] == 1) && !visited[i]) {
+                dfsComponent(i, visited, comp, size);
+            }
+        }
+    }
 
-		for (int i = 0; i < nVertices; i++) {
+    static void dfsReach(int v, boolean[] visited) {
+        visited[v] = true;
 
-			if ((adj[v][i] == 1 || adj[i][v] == 1) && !visited[i]) {
-				dfsComponent(i, comp, size);
-			}
-		}
-	}
+        for (int i = 0; i < nVertices; i++) {
+            // classic DFS: if there is an edge between vertex v and i
+            // and vertex is not visited already, perform DFS recursively
+            if (adj[v][i] == 1 && !visited[i]) {
+                dfsReach(i, visited);
+            }
+        }
+    }
 
+    static void printComponentVertices(int compNo, int[] comp, int size) {
+        System.out.print("Component " + compNo + " vertices: ");
 
-	static void reach(int v, boolean[] vis) {
-		vis[v] = true;
+        for (int i = 0; i < size; i++) {
+            System.out.print(comp[i]);
+            if (i < size - 1) System.out.print(", ");
+        }
 
-		for (int i = 0; i < nVertices; i++) {
-			if (adj[v][i] == 1 && !vis[i]) {
-				reach(i, vis);
-			}
-		}
-	}
+        System.out.println();
+    }
 
-	static void printComponentVertices(int compNo, int[] comp, int size) {
-		System.out.print("Component " + compNo + " vertices: ");
+    static void findRoots(int compNo, int[] comp, int size) {
+        System.out.print("Vertex/Vertices from which all are reachable in Component "
+                + compNo + ": ");
 
-		for (int i = 0; i < size; i++) {
-			System.out.print(comp[i]);
-			if (i < size - 1) System.out.print(", ");
-		}
+        boolean foundAtleastOne = false;
 
-		System.out.println();
-	}
+        for (int i = 0; i < size; i++) {
+            int node = comp[i];
+            boolean[] visitedComp = new boolean[nVertices];
 
-	static void findRoots(int compNo, int[] comp, int size) {
-		System.out.print("Vertex/Vertices from which all are reachable in Component "
-				+ compNo + ": ");
+            dfsReach(node, visitedComp);
 
-		boolean found = false;
+            // assume that after dfsReach, we visited all vertices
+            boolean canReachAll = true;
 
-		for (int i = 0; i < size; i++) {
-			int node = comp[i];
-			boolean[] vis = new boolean[100];
+            // check the visitedComp array if there were any vertex not visited
+            for (int j = 0; j < size; j++) {
+                if (!visitedComp[comp[j]]) {
+                    // if even any one of the vertices was not visited, disqualify
+                    canReachAll = false;
+                    break;
+                }
+            }
 
-			reach(node, vis);
+            if (canReachAll) {
+                if (foundAtleastOne) System.out.print(", ");
+                else foundAtleastOne = true;
 
-			boolean canReachAll = true;
+                System.out.print(node);
+            }
+        }
 
-			for (int j = 0; j < size; j++) {
-				if (!vis[comp[j]]) {
-					canReachAll = false;
-					break;
-				}
-			}
+        if (!foundAtleastOne) System.out.print("None");
 
-			if (canReachAll) {
-				if (found) System.out.print(", ");
-				System.out.print(node);
-				found = true;
-			}
-		}
+        System.out.println();
+    }
 
-		if (!found) {
-			System.out.print("None");
-		}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-		System.out.println();
-	}
+        System.out.print("Enter file path: ");
+        String filePath = sc.nextLine();
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+        // Read the edges from the graph and construct an adjacency matrix
+        // and put it in the adj[][] 2D array
+        // the function assumes that vertices start with number 0
+        readGraph(filePath);
 
-		System.out.print("Enter file path: ");
-		String filePath = sc.nextLine();
+        System.out.println("\nAdjacency Matrix:");
+        printAdjacencyMatrix();
+        System.out.println();
 
-		readGraph(filePath);
+        int compNo = 1;
+        boolean[] visitedMain = new boolean[100];
 
-		System.out.println("\nAdjacency Matrix:");
-		printAdjacencyMatrix();
-		System.out.println();
+        for (int i = 0; i < nVertices; i++) {
+            // for each vertex, from 0 to nVertices - 1
+            // if it is not visited
+            if (!visitedMain[i]) {
+                // initialize new array for storing vertices of the component
+                int[] comp = new int[nVertices];
+                // since java does not have pointers, this workaround allows us to
+                // set the size in a function using `size[0]`
+                int[] size = {0};
 
-		int compNo = 1;
+                // visit them and put them in a component
+                dfsComponent(i, visitedMain, comp, size);
 
-		for (int i = 0; i < nVertices; i++) {
-			if (!visited[i]) {
-				int[] comp = new int[100];
-				int[] size = {0};
+                // print the vertices in the component
+                printComponentVertices(compNo, comp, size[0]);
+                // find the root(s) of the component, ie, a
+                // vertex which has
+                findRoots(compNo, comp, size[0]);
 
+                System.out.println();
+                compNo++;
+            }
+        }
 
-				dfsComponent(i, comp, size);
-
-				printComponentVertices(compNo, comp, size[0]);
-				findRoots(compNo, comp, size[0]);
-
-				System.out.println();
-				compNo++;
-			}
-		}
-
-		sc.close();
-	}
+        sc.close();
+    }
 }
--- src/graph/GraphReachabilityCheckerNew.java
+++ src/graph/GraphReachabilityCheckerNew.java
@@ -1,18 +1,21 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 int nVertices = 0;
 int[][] adj = new int[100][100];
-boolean[] visited = new boolean[100];
 
 void readGraph(String filePath) {
     try (Scanner file = new Scanner(new File(filePath))) {
+
+        // in each line of the file: an edge (u, v) is stored like this:
+        // 2 4
+        // 4 3
         while (file.hasNextInt()) {
             int u = file.nextInt();
             int v = file.nextInt();
 
+            // directed graph, so add edge from u to v
             adj[u][v] = 1;
+
+            // vertices start from the number 0, so add 1 to max of u or v, ie
+            // if (4, 5) is found, there must be 5 + 1 = 6 vertices (0, 1, 2, 3, 4, 5)
             nVertices = Math.max(nVertices, Math.max(u, v) + 1);
         }
 
@@ -25,7 +28,7 @@ void readGraph(String filePath) {
 void printAdjacencyMatrix() {
     IO.println("\nAdjacency Matrix:");
 
-    // Column headers
+    // Print column headers
     IO.print("\t");
     for (int i = 0; i < nVertices; i++) {
         IO.print("v" + i + "\t");
@@ -33,6 +36,7 @@ void printAdjacencyMatrix() {
     IO.println();
 
     for (int i = 0; i < nVertices; i++) {
+        // Print row header
         IO.print("v" + i + "\t");
 
         for (int j = 0; j < nVertices; j++) {
@@ -42,23 +46,30 @@ void printAdjacencyMatrix() {
     }
 }
 
-void dfsComponent(int v, int[] comp, int[] size) {
+void dfsComponent(int v, boolean[] visited, int[] comp, int[] size) {
     visited[v] = true;
+
+    // add the vertex to the comp array
     comp[size[0]++] = v;
 
     for (int i = 0; i < nVertices; i++) {
+        // if there is an edge from vertex v to i or vice versa
+        // they are in the same component, and perform DFS recursively
+        // if and only if the vertex i is not visited
         if ((adj[v][i] == 1 || adj[i][v] == 1) && !visited[i]) {
-            dfsComponent(i, comp, size);
+            dfsComponent(i, visited, comp, size);
         }
     }
 }
 
-void reach(int v, boolean[] vis) {
-    vis[v] = true;
+void dfsReach(int v, boolean[] visited) {
+    visited[v] = true;
 
     for (int i = 0; i < nVertices; i++) {
-        if (adj[v][i] == 1 && !vis[i]) {
-            reach(i, vis);
+        // classic DFS: if there is an edge between vertex v and i
+        // and vertex is not visited already, perform DFS recursively
+        if (adj[v][i] == 1 && !visited[i]) {
+            dfsReach(i, visited);
         }
     }
 }
@@ -78,33 +89,36 @@ void findRoots(int compNo, int[] comp, int size) {
     IO.print("Vertex/Vertices from which all are reachable in Component "
             + compNo + ": ");
 
-    boolean found = false;
+    boolean foundAtleastOne = false;
 
     for (int i = 0; i < size; i++) {
         int node = comp[i];
-        boolean[] vis = new boolean[100];
 
-        reach(node, vis);
+        boolean[] visitedComp = new boolean[nVertices];
 
+        dfsReach(node, visitedComp);
+
+        // assume that after dfsReach, we visited all vertices
         boolean canReachAll = true;
 
+        // check the visitedComp array if there were any vertex not visited
         for (int j = 0; j < size; j++) {
-            if (!vis[comp[j]]) {
+            if (!visitedComp[comp[j]]) {
+                // if even any one of the vertices was not visited, disqualify
                 canReachAll = false;
                 break;
             }
         }
 
         if (canReachAll) {
-            if (found) IO.print(", ");
+            if (foundAtleastOne) IO.print(", ");
+            else foundAtleastOne = true;
+
             IO.print(node);
-            found = true;
         }
     }
 
-    if (!found) {
-        IO.print("None");
-    }
+    if (!foundAtleastOne) IO.print("None");
 
     IO.println();
 }
@@ -113,20 +127,37 @@ void main() {
     IO.print("Enter file path: ");
     String filePath = IO.readln();
 
+    // Read the edges from the graph and construct an adjacency matrix
+    // and put it in the adj[][] 2D array
+    // the function assumes that vertices start with number 0
     readGraph(filePath);
+
     printAdjacencyMatrix();
     IO.println();
 
     int compNo = 1;
 
+    boolean[] visitedMain = new boolean[100];
+
     for (int i = 0; i < nVertices; i++) {
-        if (!visited[i]) {
-            int[] comp = new int[100];
+        // for each vertex, from 0 to nVertices - 1
+        // if it is not visited
+        if (!visitedMain[i]) {
+
+            // initialize new array for storing vertices of the component
+            int[] comp = new int[nVertices];
+
+            // since java does not have pointers, this workaround allows us to
+            // set the size in a function using `size[0]`
             int[] size = {0};
 
-            dfsComponent(i, comp, size);
+            // visit them and put them in a component
+            dfsComponent(i, visitedMain, comp, size);
 
+            // print the vertices in the component
             printComponentVertices(compNo, comp, size[0]);
+
+            // find the root(s) of the component
             findRoots(compNo, comp, size[0]);
 
             IO.println();
